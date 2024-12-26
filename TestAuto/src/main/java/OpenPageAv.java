c926486b0e4b8d361e30996847ef27e1c18bce1e
macbookpro <macbookpro@macbookpros-mbp.cogeco.local>
2024-12-26 13:01:03 -0500

    First commit

---
+++ TestAuto/src/main/java/OpenPageAv.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenPageAv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		
+	    WebDriver dr= new ChromeDriver();
+	    dr.manage().window().maximize();
+	    dr.get("https://www.avito.ma");
+	    
+	    
+	    
+	    dr.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/nav/div/div[3]/ul/li[3]/span/span")).click();
+	    dr.findElement(By.id("private-signin-email-or-phone")).sendKeys("maghoussi.marwane@gmail.com");
+	    dr.findElement(By.id("private-signin-password")).sendKeys("Maghoussi2015");
+	    
+	    dr.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/div[2]/div/div[2]/div[3]/button")).click();
+	    
+	    System.out.println("Connexion OK");
+
+	}
+
+}
