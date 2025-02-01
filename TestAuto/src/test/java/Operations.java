108f29295d77ba4a45611088cf8caeefbae9d74d
macbookpro <macbookpro@10.65.101.39>
2025-02-01 18:17:43 -0500

    New commit

--- TestAuto/src/test/java/Operations.java
+++ TestAuto/src/test/java/Operations.java
@@ -45,6 +45,6 @@ public class Operations {
   public void CloseAvito() {
 	  
 	  dr.quit();
-	  org.testng.Reporter.log("Merci pour votre visite");
+	  org.testng.Reporter.log("Merci pour votre visite a la prochaine ");
   }
 }
